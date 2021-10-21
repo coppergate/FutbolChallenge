3aa5f0089938dfa823798d6be5a3be10b621d1b8
coppergate <coppergate@hotmail.com>
2021-10-21 15:44:11 -0600

    update some label definitions clean up a format

--- FutbolChallengeUI/Controls/SeasonDetailPanelView.xaml.cs
+++ FutbolChallengeUI/Controls/SeasonDetailPanelView.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using FutbolChallengeUI.ViewModels;
 using System.ComponentModel;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace FutbolChallengeUI.Controls
 {
 	public sealed partial class SeasonDetailPanelView : BindableUserControlBase, INotifyPropertyChanged
